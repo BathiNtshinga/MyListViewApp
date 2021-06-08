7b191816449840cf7de2918e784c28f357db4b5f
BathiNtshinga <60979642+BathiNtshinga@users.noreply.github.com>
2021-06-08 15:13:41 +0200

    Final Commit and Submission

--- app/src/main/java/com/bathi/mylistviewapp/MainActivity.java
+++ app/src/main/java/com/bathi/mylistviewapp/MainActivity.java
@@ -4,45 +4,71 @@ package com.bathi.mylistviewapp;
  * Author: Bathi Ntshinga
  * Group: Part-Time
  * Stud_Nr: 214198227
- * Due Date: 31 May 2021
  */
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.Toolbar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView lvIcons;
-    String[] proNames = {"PowerOff/On", "Playback", "Pause",
+    int [] proImages = {R.drawable.power,
+            R.drawable.play,
+            R.drawable.pause,
+            R.drawable.backward,
+            R.drawable.foward,
+            R.drawable.rec,
+            R.drawable.previous,
+            R.drawable.next,
+            R.drawable.stop,};
+
+    String [] proNames = {"PowerOff/On", "Playback", "Pause",
             "Backward", "FastForward",
             "Record", "Previous", "Next", "Stop"};
-    String[] proDescription = {"PowerOff/On Description", "Playback Description",
-            "Pause Description", "Backward Description", "FastForward Description",
-            "Record Description", "Previous Description", "Next", "StopDescription"};
-    int[] iconImages = {R.drawable.power,
-             R.drawable.play,
-             R.drawable.pause,
-             R.drawable.backward,
-             R.drawable.foward,
-             R.drawable.rec,
-             R.drawable.previous,
-             R.drawable.next,
-             R.drawable.stop,};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lvIcons = findViewById(R.id.lvIcons);
-        IconsAdapter iconsAdapter = new IconsAdapter(this, proNames, iconImages, proDescription);
-        lvIcons.setAdapter(iconsAdapter);
+
+        ListView listView = (ListView)findViewById(R.id.listView);
+        CustomAdapter customAdapter= new CustomAdapter();
+        listView.setAdapter(customAdapter);
+    }
+
+    class CustomAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return proImages.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view = getLayoutInflater().inflate(R.layout.customlayout,null);
+            ImageView imageView = (ImageView)view.findViewById(R.id.imageView);
+            TextView textView_name = (TextView)view.findViewById(R.id.textView_name);
+
+            imageView.setImageResource(proImages[i]);
+            textView_name.setText(proNames[i]);
+            return view;
+        }
     }
 
 }
-
